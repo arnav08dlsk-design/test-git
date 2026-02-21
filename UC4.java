50cc65c29ca419572a4645883f365c9fe6266cb0
Arnav Sahoo <arnav08dlsk@gmail.com>
2026-02-22 00:55:11 +0530

    Update UC4.java

--- UC4.java
+++ UC4.java
@@ -1,14 +1,15 @@
 public class UC4 {
     public static void main(String[] args) {
 
-        String[] bannerLines = {
-            String.join("", "  ***       ***      ", "****      ", "****"),
-            String.join("", " *   *     *   *     ", "*   *    ", "*"),
-            String.join("", "*     *   *     *    ", "****      ", "****"),
-            String.join("", "*     *   *     *    ", "*            ", "*"),
-            String.join("", "*     *   *     *    ", "*            ", "*"),
-            String.join("", " *   *     *   *     ", "*        ", "*   *"),
-            String.join("", "  ***       ***      ", "*         ", "***")
+String[] bannerLines = new String[7];
+
+bannerLines[0] = String.join("", "  ***       ***      ", "****      ", "****");
+bannerLines[1] = String.join("", " *   *     *   *     ", "*   *    ", "*");
+bannerLines[2] = String.join("", "*     *   *     *    ", "****      ", "****");
+bannerLines[3] = String.join("", "*     *   *     *    ", "*            ", "*");
+bannerLines[4] = String.join("", "*     *   *     *    ", "*            ", "*");
+bannerLines[5] = String.join("", " *   *     *   *     ", "*        ", "*   *");
+bannerLines[6] = String.join("", "  ***       ***      ", "*         ", "***");
         };
 
         for (String line : bannerLines) {
